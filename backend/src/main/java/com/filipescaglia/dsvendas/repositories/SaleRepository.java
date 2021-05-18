2c5cc65af2bd1296a7ca7f6b8a1587480f54abcb
Filipe Scaglia <filipecscaglia@gmail.com>
2021-05-18 17:26:20 -0300

    Pagination

---
+++ backend/src/main/java/com/filipescaglia/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.filipescaglia.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.filipescaglia.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/filipescaglia/dsvendas/services/SaleService.java
@@ -0,0 +1,30 @@
+package com.filipescaglia.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.filipescaglia.dsvendas.dto.SaleDTO;
+import com.filipescaglia.dsvendas.entities.Sale;
+import com.filipescaglia.dsvendas.repositories.SaleRepository;
+import com.filipescaglia.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(sale -> new SaleDTO(sale));
+	}
+	
+}
