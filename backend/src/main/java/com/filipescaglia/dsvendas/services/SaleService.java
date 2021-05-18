0dd4fd8323d81c4e18696b64d62da44f64a31ad5
Filipe Scaglia <filipecscaglia@gmail.com>
2021-05-18 18:02:10 -0300

    Group by search

--- backend/src/main/java/com/filipescaglia/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/filipescaglia/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.filipescaglia.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.filipescaglia.dsvendas.dto.SaleDTO;
+import com.filipescaglia.dsvendas.dto.SaleSuccessDTO;
+import com.filipescaglia.dsvendas.dto.SaleSumDTO;
 import com.filipescaglia.dsvendas.entities.Sale;
 import com.filipescaglia.dsvendas.repositories.SaleRepository;
 import com.filipescaglia.dsvendas.repositories.SellerRepository;
@@ -25,6 +29,16 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(sale -> new SaleDTO(sale));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 	
 }
