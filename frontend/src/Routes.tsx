70293d657535d8552dec88d1ccc1194f0ac3a1f5
Filipe Scaglia <filipecscaglia@gmail.com>
2021-05-19 22:37:29 -0300

    Home, navigation

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,19 +1,25 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 import Dashboard from "pages/Dashboard";
 import Home from "pages/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const Routes = () => {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/dashboard">
-                    <Dashboard />
-                </Route>
-            </Switch>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <NavBar />
+                <Switch>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                </Switch>
+                <Footer />
+            </BrowserRouter>
+        </>
     )
 }
 
